fa58f8338fa26bb51a84c2adb9d4b6075f72c75b
Mark Ehresman <435089@gmail.com>
2026-02-20 20:56:28 -0600

    Initial commit (#1)
    * Initial Changes

---
+++ agent_test.go
@@ -0,0 +1,40 @@
+package harpocrates
+
+import (
+    "testing"
+    "flag"
+)
+    
+var username = flag.String("username", "", "Username for testing")
+var password = flag.String("password", "", "Password for testing")
+var port     = flag.String("port", "", "Port number")
+
+func setup() *LinkAgent {
+    flag.Parse()
+
+    linkAgent := NewLinkAgent(*port, *username, *password)
+
+    return linkAgent
+}
+
+func TestGetAllUsers(t *testing.T) {
+    linkAgent := setup()
+
+    userCount, err := linkAgent.GetAllUsersCount()
+    if err != nil {
+        t.Fatal(err)
+    }
+
+    if userCount <= 0 {
+        t.Error("The number of users is 0.")
+    }
+}
+
+func TestCreateLocation(t *testing.T) {
+    linkAgent := setup()
+
+    err := linkAgent.CreateLocationEntry(1, 0.0000000000, 1.111111111111)
+    if err != nil {
+        t.Fatal(err)
+    }
+}
